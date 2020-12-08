70de468c76d9d7e0f3d16124335ae59ee8b9df0d
Telicia Watson <teli203@aol.com>
2020-12-07 23:35:20 -0600

    adding licenses & removing extra verbage

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,81 +1,29 @@
-function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-  }
-  return ''
-}
-
-function renderLicenseLink(license) {
-  if (license !== "None") {
-    return (
-      `\n* [License](#license)\n`
-    )
-  }
-  return ''
-}
-
-function renderLicenseSection(license) {
-  if (license !== "None") {
-    return (
-      `## License
-
-This project is licensed under the ${license} license.`
-    )
-  }
-  return ''
-}
-
+//functions to generate markdown//
 function generateMarkdown(data) {
-  return `# ${data.title}
-${renderLicenseBadge(data.license)}
 
-## Description
+  let license;
 
-${data.description}
-
-## Table of Contents 
-
-* [Installation](#installation)
-
-* [Usage](#usage)
-${renderLicenseLink(data.license)}
-* [Contributing](#contributing)
-
-* [Tests](#tests)
-
-* [Questions](#questions)
-
-## Installation
-
-To install necessary dependencies, run the following command:
-
-\`\`\`
-${data.installation}
-\`\`\`
-
-## Usage
-
-${data.usage}
-
-${renderLicenseSection(data.license)}
-  
-## Contributing
-
-${data.contributing}
-
-## Tests
-
-To run tests, run the following command:
-
-\`\`\`
-${data.test}
-\`\`\`
-
-## Questions
-
-If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
-
-`;
-}
-
-module.exports = generateMarkdown;
+  if (data.license == "Apache") {
+    license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  }
+  if (data.license == "Boost") {
+    license = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  }
+  if (data.license == "BSD") {
+    license = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  }  
+  if (data.license == "GNU") {
+    license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  }
+  if (data.license == "IBM") {
+    license = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  }
+  if (data.license == "ISC") {
+    license = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+  }
+  if (data.license == "MIT") {
+    license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  }
+  if (data.license == "Mozilla") {
+    license = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  }
